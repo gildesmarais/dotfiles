84445de1161fa3d23849d7cdecc9a6dede86a007
Gil Desmarais <gil@desmarais.de>
2017-08-29 10:05:35 +0200

    update node package install list

--- scripts/install_npm_packages.sh
+++ scripts/install_npm_packages.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+set -e
 # TODO: check if node and npm is installed
 
-NPM_PACKAGES="bower csscomb csslint git-contributors grunt grunt-cli htmlhint jshint npm npm-check-updates pmi spoof tmi uglify-js uncss"
+nvm install --lts
+brew install yarn --without-node
+
+NPM_PACKAGES="csscomb csslint git-contributors grunt grunt-cli htmlhint jshint pmi spoof tmi uglify-js uncss"
 
 npm install -g $NPM_PACKAGES
