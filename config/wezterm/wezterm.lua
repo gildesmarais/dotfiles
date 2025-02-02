9b4d290e8517286a71fb7b9396713dbc9f88f49c
Gil Desmarais <git@desmarais.de>
2025-02-02 10:32:32 +0100

    refactor(wezterm): extract font_size as variable to top of file

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,3 +1,5 @@
+local font_size = 16;
+
 local wezterm = require 'wezterm';
 local act = wezterm.action
 
@@ -64,7 +66,7 @@ wezterm.on('update-status', function(window, _)
 end)
 
 return {
-    font_size = 14.0,
+    font_size = font_size,
     color_scheme = "Tomorrow Night Eighties",
     color_schemes = {
         ["Tomorrow Night Eighties"] = {
